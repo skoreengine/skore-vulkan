ea6d98a533279c285ba7211c440dc903c9502587
Daethalus <daethalus@gmail.com>
2024-06-07 21:32:32 +0100

    vulkan files / volk / vulkan memory allocator

---
+++ build.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    var m = b.addModule("vulkan", .{
+        .root_source_file = b.path("src/vulkan.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    m.addCMacro("VK_NO_PROTOTYPES", "1");
+    m.addIncludePath(b.path("src"));
+
+    m.link_libcpp = true;
+    m.addCSourceFile(.{ .file = b.path("src/vk_mem_alloc.cpp") });
+    m.addCSourceFile(.{ .file = b.path("src/volk.c") });
+}
---
+++ src/vulkan.zig
@@ -0,0 +1,5 @@
+pub usingnamespace @cImport({
+    @cInclude("volk.h");
+    @cInclude("vulkan/vulkan.h");
+    @cInclude("vk_mem_alloc.h");
+});
